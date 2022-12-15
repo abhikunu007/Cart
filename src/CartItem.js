92cde21dc1e61764efdd3bfbe67023bc85a7e7a4
abhikunu007 <ak0879918@gmail.com>
2022-12-15 18:33:12 +0530

    Modify State

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,74 @@
 import React from "react";
 
-class CartItem extends React.Component{
+class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 777,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+    increaseQuantity = () =>  {
+        // console.log('this', this.state);
+        // SetState Form 1 --->
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // });
+
+        // setState Form 2 --->
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty +1
+            }
+        });
+    }
+
+    decreaseQuantity = () => {
+        // this.setState({
+        //     qty: this.state.qty - 1
+        // });
+
+        this.setState((nextState) => {
+            return{
+                qty: nextState.qty - 1
+            }
+        });
+    }
+
+    deleteItem = () => {
+        
+    }
+
+
+
     render() {
+        const { price, title, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
-            <img style={styles.image} />
+                    <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize: 30 }}>Phone</div>
-                    <div style={{ color: '#777' }}>Rs 777</div>
-                    <div style={{ color: '#777' }}>Qty: 7</div>
+                    <div style={{ fontSize: 30 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs: {price}</div>
+                    <div style={{ color: '#777' }}> Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img src="https://cdn-icons-png.flaticon.com/128/4315/4315609.png"
+                            alt="increase"
+                            className="action-icons"
+                            onClick={this.increaseQuantity} />
+                        <img src="https://cdn-icons-png.flaticon.com/128/4436/4436695.png" 
+                        alt="decrease"
+                         className="action-icons" 
+                         onClick={this.decreaseQuantity}/>
+                        <img src="https://cdn-icons-png.flaticon.com/128/216/216658.png"
+                         alt="delete" 
+                         className="action-icons" 
+                         onClick={this.deleteItem}/>
                     </div>
                 </div>
             </div>
