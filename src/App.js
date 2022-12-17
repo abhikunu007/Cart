a4c5869a5eaf139efc038ccb8cfd36e8961bcba3
abhikunu007 <ak0879918@gmail.com>
2022-12-17 20:30:39 +0530

    Move state from Cart to App

--- src/App.js
+++ src/App.js
@@ -3,14 +3,102 @@ import Cart from "./Cart";
 import Navbar from "./Navbar";
 
 
-function App() {
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+        products: [
+            {
+                price: 77777,
+                title: 'Phone',
+                qty: 1,
+                img: '',
+                id: 1
+            },
+            {
+                price: 777,
+                title: 'HeadPhone',
+                qty: 1,
+                img: '',
+                id: 2
+            },
+            {
+                price: 399999,
+                title: 'Laptop',
+                qty: 1,
+                img: '',
+                id: 3
+            }
+        ]
+    }
+}
+
+handleIncreaseQuantity = (product) => {
+    const {products} = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+        products: products
+    })
+}
+
+handleDecreaseQuantity = (product) => {
+    const {products} = this.state;
+    const index = products.indexOf(product);
+
+    if(products[index].qty === 0)
+    {
+        return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+        products: products
+    })
+}
+
+handleDeleteProduct = (id) => {
+    const {products} = this.state;
+
+    const items = products.filter((item) => item.id !== id);
+
+    this.setState({
+        products: items
+    });
+}
+
+getCartCount = () => {
+  const {products} = this.state;
+
+  let count = 0;
+
+  products.forEach((product) => {
+    count += product.qty;
+  });
+  return count;
+}
+
+
+
+  render() {
+    const {products} = this.state;
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar count={this.getCartCount()}/>
       <h1 id="name">CART</h1>
-      <Cart/>
+      <Cart
+      products = {products}
+      onIncreaseQuantity = {this.handleIncreaseQuantity}
+      onDecreaseQuantity = {this.handleDecreaseQuantity}
+      onDeleteProduct = {this.handleDeleteProduct}
+      />
     </div>
   );
+  }
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class CartItem extends React.Component {
+const CartItem = (props) => {
     
 
     // increaseQuantity = () =>  {
@@ -42,9 +42,9 @@ class CartItem extends React.Component {
 
 
 
-    render() {
-        const { price, title, qty } = this.props.product;
-        const {product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct} = this.props;
+    
+        const { price, title, qty } = props.product;
+        const {product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct} = props;
         return (
             <div className="cart-item">
                 <div className="left-block">
@@ -72,7 +72,6 @@ class CartItem extends React.Component {
                 </div>
             </div>
         );
-    }
 }
 
 const styles = {
